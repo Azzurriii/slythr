00e803877626027ed2499be3d49daf0f50826187
Azzurriii <vtthanh04.qb@gmail.com>
2025-06-23 16:30:25 +0700

    chore: update cors

--- internal/interface/http/middleware/cors.go
+++ internal/interface/http/middleware/cors.go
@@ -1,4 +1,3 @@
-// internal/middleware/cors.go
 package middleware
 
 import "github.com/gin-gonic/gin"
@@ -8,13 +7,17 @@ func CORS() gin.HandlerFunc {
 		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
-		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE, PATCH")
+		c.Writer.Header().Set("Access-Control-Max-Age", "86400")
 
 		if c.Request.Method == "OPTIONS" {
+			c.Header("Access-Control-Allow-Origin", "*")
+			c.Header("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE, PATCH")
+			c.Header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+			c.Header("Access-Control-Max-Age", "86400")
 			c.AbortWithStatus(204)
 			return
 		}
-
 		c.Next()
 	}
 }
